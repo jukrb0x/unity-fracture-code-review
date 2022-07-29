43bf1ce4c8f245a0fb2d3767dc3f2541bffd014e
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2022-07-29 16:48:25 +0800

    add riderflow

---
+++ Assets/Plugins/Editor/JetBrains.RiderFlow/2019.4/EntryPoint.cs
@@ -0,0 +1,34 @@
+﻿using JetBrains.RiderFlow.Core.Launchers;
+using JetBrains.RiderFlow.Core.ReEditor.Notifications;
+using JetBrains.RiderFlow.Core.UI.SearchEverywhere;
+using JetBrains.RiderFlow.Core.Utils;
+using UnityEditor;
+
+namespace JetBrains.RiderFlow.Since2019_4
+{
+    [InitializeOnLoad]
+    public class DelayedEntryPoint
+    {
+
+        static DelayedEntryPoint()
+        {
+            SearchEverywhereWindow.Settings = new SearchWindowSettings();
+            ProgressManagerOwner.ProgressManager = new LogProgressManager();
+            OpenedPrefabPreviewTrackerIntegration.Initialize();
+            UnityEditorUtils.ExecuteOnceOnUpdateCall(OnEnable);
+        }
+
+        protected static void OnEnable()
+        {
+            if (!IsPrimaryUnityProcess())
+                return;
+
+            Launcher.Run();
+        }
+        
+        private static bool IsPrimaryUnityProcess()
+        {
+            return true;
+        }
+    }    
+}
